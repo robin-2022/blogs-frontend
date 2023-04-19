6683f294cb28cdb0b9503de44922001c95c845b4
Robin <robinlaor@gmail.com>
2023-04-19 15:09:15 -0400

    feature:add login config

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,10 @@
 import '@/styles/globals.css';
+import LoginPage from './login/login';
 import 'bootstrap/scss/bootstrap.scss';
 
 export default function App({ Component, pageProps }) {
+  if (Component === LoginPage){
   return <Component {...pageProps} />
+  }
+  return <LoginPage />
 }
