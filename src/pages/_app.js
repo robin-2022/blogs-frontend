4dd6bd3c3510fb52759258d6e6d0f4acbe357df2
Robin <robinlaor@gmail.com>
2023-04-19 16:49:22 -0400

    feature: index modified

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,6 @@
 import '@/styles/globals.css';
-import LoginPage from './login/login';
 import 'bootstrap/scss/bootstrap.scss';
 
 export default function App({ Component, pageProps }) {
-  if (Component === LoginPage){
   return <Component {...pageProps} />
-  }
-  return <LoginPage />
 }
